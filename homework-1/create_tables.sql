3febc99fb2f751d96de824d9e9ccc831e362c129
Evgeny Safonov <jeka-luda@yandex.ru>
2023-12-29 21:32:14 +0300

    add sql code for create tables

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(20) NOT NULL,
+	last_name varchar(20) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date,
+	notes text NOT NULL
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(40) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date,
+	ship_city varchar(30)
+);
